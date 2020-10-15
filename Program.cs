3494cfdd2b25d545dd9304b7e11e079b204a03a4
jostalin6 <33399510+jostalin6@users.noreply.github.com>
2020-10-15 23:23:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace INKJO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //duplicate char in a array
+
+            char[] val = { 'a', 'b', 'c', 'a', 'b','c','1' };
+
+            int i = 0;
+            int j;
+            for (i=0;i<val.Length-1;i++)
+            { 
+                for(j=i+1;j<val.Length;j++)
+                {
+                    if(val[i]==val[j])
+                    {
+                        Console.WriteLine(val[i]);
+                    }
+
+                }
+
+            }
+           
+        }
+    }
+}
