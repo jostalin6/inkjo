d8578e4fda14002be50f9a12df6895322ecad20e
jostalin6 <33399510+jostalin6@users.noreply.github.com>
2020-10-16 23:06:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace INKJO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //un balanced paranthesis remove
+
+            string val = "inkjo (youtube)is a best(";
+
+            char[] arr = new char[val.Length];
+
+            int i, j;
+            for(i=0;i<arr.Length;i++)
+            {
+                arr[i] = val[i];
+            }
+
+            for(i=0;i<arr.Length;i++)
+            {
+                if(arr[i]=='(')
+                {
+                    bool flag = true;
+                    for(j=i+1;j<arr.Length;j++)
+                    {
+
+                        if(arr[j]==')')
+                        {
+                            
+                            if(flag)
+                            {
+                                Console.WriteLine("Matched");
+                                flag = false;
+                            }
+                            else
+                            {
+                                arr = arr.Where((val, idx) => idx != j).ToArray();                            }
+                        }
+
+                    }
+
+                }
+            }
+
+
+            Console.WriteLine(arr);
+        }
+    }
+}
