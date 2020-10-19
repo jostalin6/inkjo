a1649566a6994f6e13cca01a697e924e5ca049f8
jostalin6 <33399510+jostalin6@users.noreply.github.com>
2020-10-19 22:18:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace INKJO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //sorting
+
+            int [] num = { 1, 3, 4, 5, 0, 6, 7, 7 };
+
+            int i, j,temp;
+            for(i=0;i<num.Length;i++)
+            {
+                for(j=i+1;j<num.Length;j++)
+                {
+                    if(num[j]>=num[i])
+                    {
+                        temp = num[j];
+                        num[j] = num[i];
+                        num[i] = temp;
+                    }
+
+                }
+
+            }
+
+            foreach(int k in num)
+            {
+                Console.WriteLine(k);
+            }
+
+
+        }
+    }
+}
